d624648280d1cd6a0ae3e5eeb4a871eea1b20fa8
MuhammadZikri <zikrim577@gmail.com>
2022-05-12 12:41:24 +0700

    String Template

---
+++ StringTemplate.js
@@ -0,0 +1,20 @@
+// Membuat Variabel baru dan diisi langsung
+let keluar = "String Template yang diisi secara langsung untuk menampilkan output secara langsung";
+let date = "23 Desember 1978";
+let address = "Jl. Jatuh Cinta No. 59 C";
+let country = "Indonesia";
+let language = "Indonesia and English";
+let skill = "HTML, CSS, Javascript, Balsamiq, MySQL, MongoDB";
+
+// Memanggil Variabel
+let tanggalahir = `Tanggal Lahir : ${date}`;
+let alamat = `Alamat : ${address}`;
+let negara = `Warga Negara : ${country}`;
+let ahli = `Keahlian : ${skill}`;
+
+// Output
+console.warn(keluar);
+console.warn(tanggalahir);
+console.warn(alamat);
+console.warn(negara);
+console.warn(ahli);
